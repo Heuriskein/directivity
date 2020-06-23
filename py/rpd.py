e4c73ca32a82dab98bb82746c03a1c7917324016
Ryan Mills <rmills@blizzard.com>
2020-06-22 22:00:58 -0700

    Fix rightmost label

--- py/rpd.py
+++ py/rpd.py
@@ -30,11 +30,12 @@ q_s = (transpose(phi), transpose(q))
 #Dirplot(q_s(:,1)*180/pi,q_s(:,2),'r-',[0 -30 5]);
 ax = plt.subplot('111', projection='polar')
 ax.plot(q_s[0], q_s[1], 'r-')
+
 ax.set_rticks(range(-30, 1, 6))
 ax.set_theta_offset(pi/2.0)
 ax.set_thetalim(thetamin=90, thetamax=-90)
 ax.set_theta_direction(-1)
-ax.set_thetagrids(range(-90, 90, 10),
+ax.set_thetagrids(range(-90, 91, 10),
 	labels=["-90", "", "", "-60", "", "", -30, "", "", 0, "", "", 30, "", "", 60, "", "", 90])
 ax.set_rmax(0)
 ax.set_rmin(-30)
